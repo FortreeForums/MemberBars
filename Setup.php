00d0644310d1f11f62094c228fed73f81a846d7e
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-09-13 23:33:23 +0100

    Add GPLv3 license

--- Setup.php
+++ Setup.php
@@ -1,5 +1,28 @@
 <?php
 
+//  ▄▄▄·  ▄▄▄· ▄▄▄· ▄▄▄▄▄ ▄ .▄ ▄· ▄▌
+// ▐█ ▀█ ▐█ ▄█▐█ ▀█ •██  ██▪▐█▐█▪██▌
+// ▄█▀▀█  ██▀·▄█▀▀█  ▐█.▪██▀▐█▐█▌▐█▪
+// ▐█ ▪▐▌▐█▪·•▐█ ▪▐▌ ▐█▌·██▌▐▀ ▐█▀·.
+//  ▀  ▀ .▀    ▀  ▀  ▀▀▀ ▀▀▀ ·  ▀ •
+//  https://fortreeforums.xyz
+//  Licensed under GPL-3.0-or-later 2021
+//
+//  This file is part of [AP] Member Bars ("MemBars").
+//
+//  MemBars is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MemBars is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MemBars.  If not, see <https://www.gnu.org/licenses/>.
+
 namespace apathy\MemberBars;
 
 use XF\AddOn\AbstractSetup;
